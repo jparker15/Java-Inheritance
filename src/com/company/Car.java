93d5cbb9c4d8bed29941b68dbbf2ec8d9e328dd6
Jeremy <jeremyeparker@yahoo.com>
2020-10-21 23:59:51 -0400

    first commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Car {
+
+    private String color;
+    private String model;
+    private String make;
+    private int year;
+    private boolean isUsed;
+
+    public Car (String color, String make, String model, Boolean isUsed){
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        this.isUsed = isUsed;
+    }
+
+}
+
